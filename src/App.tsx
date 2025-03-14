1ce0d4c1c86db2ff6d1d46bf5cd4f19040fa55f2
Deflinger <cesaralejandrovirgenherrera@gmail.com>
2025-03-14 11:40:25 -0600

    Custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,27 @@
-import { useEffect, useState } from 'react'
+//import { useEffect, useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+import { useFetch } from './components/hooks';
 //import { Button } from './components/Index'
 
+const url = "https://api.example.com/data";
+
+interface Data {
+  Name:string,
+  lastName:string,
+  age:number
+}
 
 function App() {
   //const [count, setCount] = useState(0)
-  const [data,setData] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState("")
-
-  const consoleLoader = (loadingValue : boolean) =>{
-    setLoading (loadingValue)
-    console.info(loading)
-  }
-
-  const fetchData = async () => {
-    consoleLoader(true)
-    try{
-      const response = await fetch("https://api.example.com/data")
-
-      if(!response.ok){
-        throw new Error("Error al obtener datos")
-      }
-
-      const jsonData = await response.json()
-      setData(jsonData)
-    } catch (err){
-      setError(err as string)
-    }finally{
-      consoleLoader(false)
-    }
-  }
-
-  useEffect(()=>{
-    fetchData()
-    return () => {}
-  },[])
-
+  const {data,loading,error} = useFetch<Data>(url)
   if(loading){
     return <div>cargando...</div>
   }
 
   if (error){
-    return <div>Ups! parece que hubo un error: {error}</div>
+    return <div>Ups! parece que hubo un error: {error.message}</div>
   }
   /*
   const countMore = () => {
