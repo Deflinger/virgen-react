8c2302bd6b175109ed96bb97cd2f325f1d759d06
Deflinger <cesaralejandrovirgenherrera@gmail.com>
2025-04-10 15:43:34 -0600

    React route 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
+import { ReactNode } from 'react'
 import './App.css'
-import { getCharacter,} from './services/api.service'
-import { Character,} from './components/models';
-import { useApi } from './hooks/useApi';
 
-function App() {
-/*const [data , setData] = useState<Character>(emptyCharacter);
-
-const fetchCharacter = async () => {
-    const result = await getCharacter();
-    setData(result.data)
+interface Props{
+    children: ReactNode;
 }
-useEffect(()=> {
-    fetchCharacter()
-    
-},[])
-*/
-
-const {loading, error, data, fetch} = useApi<Character,number>(getCharacter,{autoFetch:true,params:1})
-//const {loading, error, data, fetch} = useApi<Character,number>(getCharacter)
-if(loading){
-    return (<p>Cargando</p>)
-}
-if(error){
-    return(<p>Ups {error.message}</p>)
-}
-
- return ( 
-  <>
-    {JSON.stringify(data)}
-    <button onClick={()=> fetch(2)}></button>
-  </>
+function App({children}:Props) {
+ return (
+    <>
+    <p>
+        NavBar
+    </p>
+    {children}
+    <p>
+        footer
+    </p>
+    </>
  )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import { ModalProvider } from './components/Modal/context/ModalContext.tsx'
-import ErrorBoundary from './ErrorBoundary.tsx'
 import { initAxios } from './services/axios.service.ts'
+import AppHookContainer from './AppHookContainer.tsx'
 
 initAxios();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <ModalProvider>
-        <App />
-      </ModalProvider>
-    </ErrorBoundary>
+        <AppHookContainer />
   </React.StrictMode>,
 )
